5c7753488b3b9428a6158fadc6f1e8f607277547
Om Zalavadiya <omzalavadiya301002@gmail.com>
2026-05-30 15:37:01 +0530

    Day 12 Task (1): Deploy frontend and connect Render backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,8 +28,9 @@ exports.login=async(req,res)=>{
         if(!isMatch){
             return res.status(400).json({message:'Invalid credentials'});
         }
+        const isProduction = process.env.NODE_ENV === "production";
         const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
-        res.cookie('token',token,{httpOnly:true, secure:true, sameSite:'lax', maxAge:7*24*60*60*1000});
+        res.cookie('token',token,{httpOnly:true, secure: isProduction, sameSite: isProduction ? 'none' : 'lax', maxAge:7*24*60*60*1000});
         res.json({message:'Login successful', user:{id:user._id, name:user.name, email:user.email}});
     }catch(err){
         res.status(500).json({message:'Login failed'});
